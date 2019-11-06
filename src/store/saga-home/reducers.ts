5e13d1e33ad7e8271839b81530b2c69b0b5d2d48
josephmannis <annis.jo@husky.neu.edu>
2019-11-06 15:07:14 -0500

    Broken redux.

---
+++ src/store/saga-home/reducers.ts
@@ -0,0 +1,81 @@
+import { HomeState, UserProject, PublishedProject } from './StateTypes';
+import { HomeActionTypes, HomeActions } from './ActionTypes';
+
+
+// State types
+export interface HomeState {
+    featuredProjects: PublishedProject[];
+    userProjects: UserProject[];
+    creatingProject: boolean;
+ }
+ 
+ export interface UserProject {
+     projectId: string;
+     projectTitle: string;
+ }
+ 
+ export interface PublishedProject {
+     projectId: string; 
+     projectTitle: string;
+     coverImageUrl: string;
+     topics: string[];
+ }
+
+
+ // Reducer functions
+const initialState: HomeState = {
+    featuredProjects: [],
+    userProjects: [],
+    creatingProject: false
+}
+
+export function homeReducer(
+    state = initialState,
+    action: HomeActionTypes
+): HomeState {
+    switch(action.type) {
+        case HomeActions.FETCH_FEATURED_PROJECTS:
+            return {
+                ...state,
+                featuredProjects: [...state.featuredProjects, action. as PublishedProject[]],
+            };
+        case HomeActions.FETCH_USER_PROJECTS:
+            return {
+                ...state,
+                userProjects: action.payload,
+            };
+        case HomeActions.CREATE_USER_PROJECT:
+            return {
+                ...state,
+                creatingProject: action.payload
+            };
+        default: 
+            return state;
+
+    };
+}
+
+
+function getFeaturedProjects(): PublishedProject[] {
+    let state: PublishedProject[] = [];
+
+    state.push({
+        projectId: 'fakeid',
+        projectTitle: 'title',
+        coverImageUrl: 'img',
+        topics: ['example', 'example']
+    });
+
+    return state;
+}
+
+function getUserProjects(): UserProject[] {
+    let state: UserProject[] = [];
+
+    state.push({
+        projectId: 'fakeid',
+        projectTitle: 'title',
+    });
+
+    return state;
+}
