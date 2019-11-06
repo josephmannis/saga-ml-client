41f9da2f67c7b41fc0a0bef1b7e3f7fae055965e
josephmannis <annis.jo@husky.neu.edu>
2019-11-06 01:11:23 -0500

    Scaffolded out major components. This will provide us the minimum required functionality for the two flows.

---
+++ src/components/SagaHome/ProjectDiscovery/ProjectDiscoveryHome.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IProjectModel } from '../../model';
+
+interface IProjectDiscoveryProps {
+    projects: IProjectModel[];
+}
+
+interface IProjectDiscoveryState {
+
+}
+
+class ProjectDiscoveryHome extends React.Component<IProjectDiscoveryProps, IProjectDiscoveryState> {
+    constructor(props: IProjectDiscoveryProps) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+}
+
+
+export default Component
---
+++ src/connectors/ConnectedProjectDiscovery.tsx
@@ -0,0 +1,7 @@
+import react from 'react'
+
+const ConnectedProjectDiscovery: React.FC = () => {
+
+}
+
+export default ConnectedProjectDiscovery
