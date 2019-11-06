144884fbe15abf9347e08752f7621e56250c4054
Dhruv Sheth <dhruvnsheth@gmail.com>
2019-11-06 15:58:47 -0500

    deleted footer, placeholder header

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,8 +1,13 @@
-import react from 'react'
+import React from 'react'
 
 interface IHeaderProps {
 
 }
-const Header: React.FC<IHeaderProps> = props => {
 
-}
+const Header: React.FC<IHeaderProps> = props => {
+     return (
+         <p>Hello from the header</p>
+     );
+};
+
+export default Header;
