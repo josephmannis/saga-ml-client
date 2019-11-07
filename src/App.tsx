cfe252557782faef1f9629eccc44600202cbbfa0
Dhruv Sheth <dhruvnsheth@gmail.com>
2019-11-06 21:10:01 -0500

    wip headerbar with no functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.css';
 import ProjectDashboard from './components/project-dashboard/ProjectDashboard';
 import 'bootstrap/dist/css/bootstrap.css';
 import { IProject } from './components/model';
+import Header from "./components/Global/Header";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
       <ProjectDashboard project={ getSampleProject() }/>
     </div>
   );
-}
+};
 
 function getSampleProject(): IProject {
   return (
