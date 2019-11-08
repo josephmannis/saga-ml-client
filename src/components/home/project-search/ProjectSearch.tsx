5b01a78f8e73b671726178ead33a4c8af635b71f
josephmannis <annis.jo@husky.neu.edu>
2019-11-08 01:43:46 -0500

    Made search results fake and link somewhere

--- src/components/home/project-search/ProjectSearch.tsx
+++ src/components/home/project-search/ProjectSearch.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import { PublishedProject } from '../../../state/saga-home/datatypes';
+import ProjectResult from './ProjectResult';
+import { Col, Row } from 'react-bootstrap';
 
 
 interface IProjectSearchProps {
-
+    projectResults: PublishedProject[];
 }
 
-interface IProjectSearchState {
-
-}
-
-class ProjectSearch extends React.Component<IProjectSearchProps, IProjectSearchState> {
-    constructor(props: IProjectSearchProps) {
-        super(props);
-        this.state = {
-        }
+// Bad and fake news
+const projectResults = [
+    {
+        projectId: 'fake',
+        projectTitle: 'Presidential Debates of Donald Trump',
+        coverImageUrl: 'no',
+        topics: ['trump', 'healthcare']
     }
+]
 
-    public render() {
-        return (
-            <div>Search</div>
-        );
-    }
+const ProjectSearch: React.FC<IProjectSearchProps> = props => {
+    return (
+        <Row className='justify-content-center'>
+            <Col className='p-3 text-left' xs='6'>
+                <h2 className='font-weight-bold'>Search Results</h2>
+                <p>1 Result</p>
+                {projectResults.map((result) => <ProjectResult projectTitle={result.projectTitle} projectDescription={'the one and only study about donald trump'} projectTopics={result.topics} />)}
+            </Col>
+        </Row>
+    )
 }
 
 export default ProjectSearch;
